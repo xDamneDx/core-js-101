cb042520572000868d67041f14bce27fb6717cb9
xDamneDx <xdamnedx@gmail.com>
2022-11-25 21:45:42 +0200

    feat: implement isString

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -269,8 +269,8 @@ function encodeToRot13(/* str */) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  return value ? typeof value[0] === 'string' : false;
 }
 
 /**
