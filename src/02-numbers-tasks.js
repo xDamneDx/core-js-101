894cf50167987cf5d00557ec562c51f6b4e7f933
xDamneDx <xdamnedx@gmail.com>
2022-11-28 11:35:48 +0200

    feat: implement getDistanceBetweenPoints

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -68,8 +68,10 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(/* x1, y1, x2, y2 */) {
-  throw new Error('Not implemented');
+function getDistanceBetweenPoints(x1, y1, x2, y2) {
+  const a = Math.abs(x1) + Math.abs(x2);
+  const b = Math.abs(y1) + Math.abs(y2);
+  return Math.sqrt(a * a + b * b);
 }
 
 /**
