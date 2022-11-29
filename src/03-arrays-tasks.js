40575f0f20c6e2f63f186c6220dead8e65b46730
xDamneDx <xdamnedx@gmail.com>
2022-11-29 17:44:30 +0200

    feat: implement insertItem

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -147,8 +147,8 @@ function getStringsLength(arr) {
  *    [ 1, 3, 4, 5 ], 2, 1  => [ 1, 2, 3, 4, 5 ]
  *    [ 1, 'b', 'c'], 'x', 0  => [ 'x', 1, 'b', 'c' ]
  */
-function insertItem(/* arr, item, index */) {
-  throw new Error('Not implemented');
+function insertItem(arr, item, index) {
+  return arr.splice(index, 0, item);
 }
 
 /**
