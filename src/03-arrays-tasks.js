c812eb92e3514dcab0a2b9f539d31663d98a58f7
xDamneDx <xdamnedx@gmail.com>
2022-12-01 22:10:28 +0200

    feat: implement propagateItemsByPositionIndex

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -276,8 +276,12 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr
+    .map((el, index) => Array(index + 1)
+      .fill(null)
+      .map(() => el))
+    .flat();
 }
 
 /**
