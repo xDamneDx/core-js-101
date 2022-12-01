29305ef06f5a7de13accaf5eb92b978254a5abf9
xDamneDx <xdamnedx@gmail.com>
2022-12-01 22:11:37 +0200

    feat: implement get3TopItems

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -297,8 +297,8 @@ function propagateItemsByPositionIndex(arr) {
  *   [ 1,2,3,4,5,6,7,8,9,10 ] => [ 10, 9, 8 ]
  *   [ 10, 10, 10, 10 ] => [ 10, 10, 10 ]
  */
-function get3TopItems(/* arr */) {
-  throw new Error('Not implemented');
+function get3TopItems(arr) {
+  return arr.sort((a, b) => b - a).splice(0, 3);
 }
 
 /**
