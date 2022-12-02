cd1980034691c7c420c3e1a78a07e8d5f3949642
xDamneDx <xdamnedx@gmail.com>
2022-12-02 18:15:55 +0200

    feat: implement getIdentityMatrix

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -503,8 +503,15 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array(n)
+    .fill(Array(n).fill(0))
+    .map((row, rowIndex) => row.map((col, colIndex) => {
+      if (rowIndex === colIndex) {
+        return 1;
+      }
+      return col;
+    }));
 }
 
 /**
