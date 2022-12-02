f9c9e7ebead58ccc92a60e126987c468dc425153
xDamneDx <xdamnedx@gmail.com>
2022-12-02 18:13:04 +0200

    feat: implement sortCitiesArray

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -467,8 +467,22 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    if (a.country > b.country) {
+      return 1;
+    }
+    if (a.country < b.country) {
+      return -1;
+    }
+    if (a.city > b.city) {
+      return 1;
+    }
+    if (a.city < b.city) {
+      return -1;
+    }
+    return 0;
+  });
 }
 
 /**
