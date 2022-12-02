080168a54fdf4bd3f4c3f0daed493a42bdca1f5b
xDamneDx <xdamnedx@gmail.com>
2022-12-02 18:09:47 +0200

    feat: implement findAllOccurrences

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -413,8 +413,17 @@ function getFalsyValuesCount(arr) {
  *    [ null, undefined, null ], null => 2
  *    [ true, 0, 1, 'true' ], true => 1
  */
-function findAllOccurrences(/* arr, item */) {
-  throw new Error('Not implemented');
+function findAllOccurrences(arr, item) {
+  let occurrences = 0;
+
+  arr.map((el) => {
+    if (el === item) {
+      occurrences += 1;
+    }
+    return el;
+  });
+
+  return occurrences;
 }
 
 /**
