4b227e8c507205af46545476dad28775dadd5650
xDamneDx <xdamnedx@gmail.com>
2022-12-02 18:17:38 +0200

    feat: implement getIntervalArray

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -527,8 +527,15 @@ function getIdentityMatrix(n) {
  *     0, 100 => [ 0, 1, 2, ..., 100 ]
  *     3, 3   => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  let num = start;
+  return Array(end - start + 1)
+    .fill(null)
+    .map(() => {
+      const el = num;
+      num += 1;
+      return el;
+    });
 }
 
 /**
