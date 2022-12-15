e03c880f53a508e3912b3ca3733b390b0b987333
xDamneDx <xdamnedx@gmail.com>
2022-12-15 22:33:24 +0200

    implement willYouMarryMe

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -5,7 +5,6 @@
  *                                                                                                *
  ************************************************************************************************ */
 
-
 /**
  * Return Promise object that is resolved with string value === 'Hooray!!! She said "Yes"!',
  * if boolean value === true is passed, resolved with string value === 'Oh no, she said "No".',
@@ -28,10 +27,17 @@
  *      .catch((error) => console.log(error.message)) // 'Error: Wrong parameter is passed!
  *                                                    //  Ask her again.';
  */
-function willYouMarryMe(/* isPositiveAnswer */) {
-  throw new Error('Not implemented');
+function willYouMarryMe(isPositiveAnswer) {
+  return new Promise((resolve, reject) => {
+    if (typeof isPositiveAnswer !== 'boolean') {
+      reject(new Error('Wrong parameter is passed! Ask her again.'));
+    } else {
+      resolve(
+        isPositiveAnswer ? 'Hooray!!! She said "Yes"!' : 'Oh no, she said "No".',
+      );
+    }
+  });
 }
-
 
 /**
  * Return Promise object that should be resolved with array containing plain values.
