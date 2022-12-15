7c7ed01cf81864404b11f704acff3303b35ebaf4
xDamneDx <xdamnedx@gmail.com>
2022-12-15 22:34:30 +0200

    implement processAllPromises

--- src/06-promises-tasks.js
+++ src/06-promises-tasks.js
@@ -54,8 +54,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
