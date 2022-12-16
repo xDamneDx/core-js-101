791c4d6f07ef1bfe81d70a712cbcc3a7587c0dad
xDamneDx <xdamnedx@gmail.com>
2022-12-16 13:15:38 +0200

    implement getSumBetweenNumbers

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -63,8 +63,12 @@ function getFactorial(n) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2) {
+  if (n1 === n2) {
+    return n2;
+  }
+
+  return n1 + getSumBetweenNumbers(n1 + 1, n2);
 }
 
 /**
