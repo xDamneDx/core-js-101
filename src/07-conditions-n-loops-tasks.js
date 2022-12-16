49f016dcde5a23c00841bb19a3974ee7fce87c9b
xDamneDx <xdamnedx@gmail.com>
2022-12-16 13:14:40 +0200

    implement getFactorial

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -44,8 +44,11 @@ function getFizzBuzz(num) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  if (n === 1) {
+    return 1;
+  }
+  return n * getFactorial(n - 1);
 }
 
 /**
